cd583ccb3f7fd221844a59641b5fb6863cd49926
Maxim <maximagaffonnovv@gmail.com>
2024-06-10 10:23:28 +0300

    add

--- src/components/kol/kol.jsx
+++ src/components/kol/kol.jsx
@@ -23,14 +23,14 @@ const Rotate = () => {
         <div className="roulette">
             <div className="container" ref={containerRef}>
                 <button id='spin'></button>
-                <div className="one"><span>Скидка</span></div>
-                <div className="two"><span>Скидка</span></div>
-                <div className="three"><span>Скидка</span></div>
-                <div className="four"><span>Скидка</span></div>
-                <div className="five"><span>Скидка</span></div>
-                <div className="six"><span>Скидка</span></div>
-                <div className="seven"><span>Скидка</span></div>
-                <div className="eight"><span>Скидка</span></div>
+                <div className="one"><span>Москитная сетка в подарок</span></div>
+                <div className="two"><span>Скидка 5%</span></div>
+                <div className="three"><span>Бесплатная доставка</span></div>
+                <div className="four"><span>В этот раз не повезло</span></div>
+                <div className="five"><span>Скидка 3%</span></div>
+                <div className="six"><span>Мультифункциональное <br />стекло в подарок</span></div>
+                <div className="seven"><span>Скидка 2%</span></div>
+                <div className="eight"><span>Гребенка для окна в подарок</span></div>
             </div>
         </div>
        <span className="title">Получите бонус сейчас!</span>
